3ad199a0e90a7ddbdcefa6304a6886b47092fff3
ruth610 <106428054+ruth610@users.noreply.github.com>
2024-05-31 08:11:45 +0300

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,18 @@
+package assignment_1;
+
+public class Node {
+	
+	Task task;
+    Node next;
+
+    public Node(Task task) {
+        this.task = task;
+        this.next = null;
+    }
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
